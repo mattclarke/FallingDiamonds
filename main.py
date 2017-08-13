04154f3d28bd3663bdd57fa90291860b67dfe171
Matt Clarke <mjclarke01@yahoo.co.uk>
2017-08-13 17:33:43 +0100

    Progress made!

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 # https://code.google.com/codejam/contest/2434486/dashboard#s=p1
+from collections import OrderedDict
 
 SMALL_N = 20
 BIG_N = 1000000
 
 
 def calculate_triangles_up_to_n(n):
-    triangles = []
+    triangles = OrderedDict()
     # Height in diamonds at x = 0 goes up in twos starting from zero
     h = 0
     while True:
         if h == 0:
-            triangles.append((0, 1))
+            triangles[0] = 1
         else:
-            # Number of whole diamonds wide = height + 1
+            # Number of whole diamonds = (h + 1) + (h) + (h - 1) ... + (1)
             w = h + 1
+            # Both these calculations work but which is quicker?
+            # ans = sum(x for x in range(w + 1))
             ans = ((w * w) - w)/2 + w
-            triangles.append((h, ans))
+            triangles[h] = ans
             if ans >= n:
                 break
         h += 2
@@ -29,12 +32,60 @@ class Solver:
         self.num = num
 
     def calculate_height(self, num):
-        for h, n in TRIANGLES:
-            print(h, n)
-            if n >= num:
+        prev = 0
+
+        for h, n in TRIANGLES.items():
+            if n > num:
+                return prev
+            prev = h
+
+    def get_max_edge_height(self, num):
+        # Edge height is the same as the central height
+        # for the next biggest triangle
+        # because we don't include the tip diamond as part of the edge
+        for h, n in TRIANGLES.items():
+            if n > num:
                 return h
 
+    def create_columns(self):
+        h = self.get_max_edge_height(self.num)
+        left_col = [False] * h
+        right_col = [False] * h
+        return left_col, right_col
+
+    def populate_columns(self, left_col, right_col, num_free):
+        def _fill_column(col, num_free):
+            while True:
+                if num_free == 0:
+                    return 0
+                if col[-1]:
+                    # All of the column is full
+                    return num_free
+                # Fill the first unfilled space
+                col[col.index(False)] = True
+                num_free -= 1
+        # Fill the right-hand column first
+        num_free = _fill_column(right_col, num_free)
+        _fill_column(left_col, num_free)
+        return left_col, right_col
+
+    def shift_diamond_left(self, left_col, right_col):
+        # Should throw if cannot shift further left?
+        pass
+
+    def calculate_probability(self):
+        h = self.get_max_edge_height(self.num)
+        left_col = [False] * h
+        right_col = [False] * h
+        free = self.get_number_free()
+
+    def get_number_free(self):
+        h = self.calculate_height(self.num)
+        return self.num - TRIANGLES[h]
+
     def get_probability(self, x, y):
+        # Due to symmetry can ignore sign
+        x = abs(x)
         if self.calculate_height(self.num) < y:
             return 0.0
         return 1.0
--- tests.py
+++ tests.py
@@ -8,12 +8,12 @@ class FallingDiamondsTest(unittest.TestCase):
 
     def test_calculate_triangles_matches_hand_calculated_values(self):
         ans = calculate_triangles_up_to_n(66)
-        self.assertEqual(1, ans[0][1])
-        self.assertEqual(6, ans[1][1])
-        self.assertEqual(15, ans[2][1])
-        self.assertEqual(28, ans[3][1])
-        self.assertEqual(45, ans[4][1])
-        self.assertEqual(66, ans[5][1])
+        self.assertEqual(1, ans[0])
+        self.assertEqual(6, ans[2])
+        self.assertEqual(15, ans[4])
+        self.assertEqual(28, ans[6])
+        self.assertEqual(45, ans[8])
+        self.assertEqual(66, ans[10])
 
     def test_one_diamond_hits_zero_zero(self):
         s = Solver(1)
@@ -23,9 +23,95 @@ class FallingDiamondsTest(unittest.TestCase):
         s = Solver(1)
         self.assertEqual(0, s.calculate_height(1))
 
+    def test_calculate_height_for_five_diamonds_is_zero(self):
+        s = Solver(1)
+        self.assertEqual(0, s.calculate_height(5))
+
+    def test_calculate_height_for_six_diamonds_is_two(self):
+        s = Solver(1)
+        self.assertEqual(2, s.calculate_height(6))
+
+    def test_calculate_height_for_fourteen_diamonds_is_two(self):
+        s = Solver(1)
+        self.assertEqual(2, s.calculate_height(14))
+
+    def test_calculate_height_for_fifteen_diamonds_is_four(self):
+        s = Solver(1)
+        self.assertEqual(4, s.calculate_height(15))
+
     def test_one_diamond_misses_zero_two(self):
-            s = Solver(1)
-            self.assertEqual(0.0, s.get_probability(0, 2))
+        s = Solver(1)
+        self.assertEqual(0.0, s.get_probability(0, 2))
+
+    def test_three_diamonds_hits_zero_zero(self):
+        s = Solver(3)
+        self.assertEqual(1.0, s.get_probability(0, 0))
+
+    def test_edge_height_for_seven_to_fourteen_is_four(self):
+        s = Solver(7)
+        self.assertEqual(4, s.get_max_edge_height(7))
+        s = Solver(14)
+        self.assertEqual(4, s.get_max_edge_height(14))
+
+    def test_edge_height_for_sixteen_to_twentyseven_is_six(self):
+        s = Solver(16)
+        self.assertEqual(6, s.get_max_edge_height(16))
+        s = Solver(27)
+        self.assertEqual(6, s.get_max_edge_height(27))
+
+    def test_number_of_free_diamonds_is_correct(self):
+        s = Solver(3)
+        self.assertEqual(2, s.get_number_free())
+        s = Solver(5)
+        self.assertEqual(4, s.get_number_free())
+        s = Solver(7)
+        self.assertEqual(1, s.get_number_free())
+        s = Solver(14)
+        self.assertEqual(8, s.get_number_free())
+        s = Solver(16)
+        self.assertEqual(1, s.get_number_free())
+        s = Solver(27)
+        self.assertEqual(12, s.get_number_free())
+
+    def test_create_columns_for_various_n_gives_correct_length(self):
+        s = Solver(3)
+        ans = s.create_columns()
+        self.assertEqual(2, len(ans[0]))
+        self.assertEqual(2, len(ans[1]))
+        s = Solver(14)
+        ans = s.create_columns()
+        self.assertEqual(4, len(ans[0]))
+        self.assertEqual(4, len(ans[1]))
+        s = Solver(27)
+        ans = s.create_columns()
+        self.assertEqual(6, len(ans[0]))
+        self.assertEqual(6, len(ans[1]))
+
+    def test_populate_columns_for_height_four_and_eight_diamonds_fills_both(self):
+        s = Solver(14)
+        l, r = s.create_columns()
+        l, r = s.populate_columns(l, r, 8)
+        self.assertEqual([True, True, True, True], l)
+        self.assertEqual([True, True, True, True], r)
+
+    def test_populate_columns_for_height_four_and_six_diamonds_fills_right_and_half_left(self):
+        s = Solver(14)
+        l, r = s.create_columns()
+        l, r = s.populate_columns(l, r, 6)
+        self.assertEqual([True, True, False, False], l)
+        self.assertEqual([True, True, True, True], r)
+
+    def test_shifting_left_for_height_four_and_six_diamonds_equally_fills_right_and_left(self):
+        s = Solver(14)
+        l, r = s.create_columns()
+        l, r = s.populate_columns(l, r, 6)
+        l, r = s.shift_diamond_left(l, r)
+        self.assertEqual([True, True, True, False], l)
+        self.assertEqual([True, True, True, False], r)
+
+    # def test_three_diamonds_hits_two_zero_occasionally(self):
+    #     s = Solver(3)
+    #     self.assertEqual(0.75, s.get_probability(2, 0))
 
 
 # 7
